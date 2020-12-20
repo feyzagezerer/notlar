4eb98f15dbb731f03a885a069ba552b9b9c2c720
feyzagezerer <feygfey9@gmail.com>
2020-12-21 02:10:42 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/feyzagezerer/notlar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.feyzagezerer.notlar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/database_helper.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper _databaseHelper;
+  static Database _database;
+
+  factory DatabaseHelper() {
+    //return kullanacaksan factory de
+
+    if (_databaseHelper == null) {
+      _databaseHelper = DatabaseHelper._internal();
+      return _databaseHelper;
+    } else {
+      return _databaseHelper;
+    }
+  }
+
+  DatabaseHelper._internal();
+
+  Future<Database> _getDatabase() async {
+    if (_database == null) {
+      _database = await _initializeDatabase();
+      return _database;
+    } else {
+      return _database;
+    }
+  }
+
+  _initializeDatabase() async {
+    Database _db;
+    var databasesPath = await getDatabasesPath();
+    var path = join(databasesPath, "notlar.db");
+
+// Check if the database exists
+    var exists = await databaseExists(path);
+
+    if (!exists) {
+      // Should happen only the first time you launch your application
+      print("Creating new copy from asset");
+
+      // Make sure the parent directory exists
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {}
+
+      // Copy from asset
+      ByteData data = await rootBundle.load(join("assets", "notlar.db"));
+      List<int> bytes =
+          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+      // Write and flush the bytes written
+      await File(path).writeAsBytes(bytes, flush: true);
+    } else {
+      print("Opening existing database");
+    }
+// open the database
+    _db = await openDatabase(path, readOnly: true);
+  }
+}
